e73291bfb0b2399acca0832333bb5e39d3089663
Fachru Rozi <arieffachrurozi@gmail.com>
2016-01-29 11:19:34 +0700

    Add New

---
+++ app/src/main/java/com/fachru/sigmamobile/fragment/interfaces/OnSetSoHeadListener.java
@@ -0,0 +1,11 @@
+package com.fachru.sigmamobile.fragment.interfaces;
+
+import com.fachru.sigmamobile.model.SoHead;
+
+/**
+ * Created by fachru on 28/01/16.
+ */
+public interface OnSetSoHeadListener {
+    void onSetSoHead(SoHead soHead);
+    void unSetSoHead();
+}
---
+++ app/src/main/java/com/fachru/sigmamobile/fragment/interfaces/OnSetSoItemListener.java
@@ -0,0 +1,8 @@
+package com.fachru.sigmamobile.fragment.interfaces;
+
+/**
+ * Created by fachru on 28/12/15.
+ */
+public interface OnSetSoItemListener {
+    void unSetDoItem();
+}
